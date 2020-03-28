03e2c77c2809ac8759dfe55c57d03417b466d67f
Roberto Livi <robertomlivi@outlook.com>
2020-03-28 21:04:34 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+  
+  def call(env)
+    resp = Rack::Response.new
+    
+    time = Time.now.hour
+    
+    if time < 12
+      puts resp.write "Good Morning!"
+    else
+      puts resp.write "Good Afternoon!"
+    end
+    
+  end
+  
+  resp.finish
+  
+end
